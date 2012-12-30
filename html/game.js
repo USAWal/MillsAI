9c835d8770eba50300a4ece0271294b465fe2a39
Valeriy Utyaganov <usawal@gmail.com>
2012-12-30 08:57:58 +0400

    Implemented dropping coins on the board

--- html/game.js
+++ html/game.js
@@ -1,54 +1,58 @@
 var SET_STAGE = 0;
+var MOVE_STAGE = 1;
+var WHITE_PLAYER = 0
+var BLACK_PLAYER = 1;
 
 function Board(){
-  this.pieces = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  this.pieces = new Array();
   this.stage = SET_STAGE;
-  this.player_mode = 0;
+  this.player_mode = WHITE_PLAYER;
   this.waits = false;
 }
 
-Board.prototype.place_value = function(index) { return this.pieces[index]; }
+Board.prototype.bind = function(piece, index) {
+  piece.value = 0;
+  piece.white_light = piece.childNodes[1].childNodes[1].childNodes[3];
+  piece.black_light = piece.childNodes[1].childNodes[1].childNodes[1];
+  piece.gold_coin   = piece.childNodes[3].childNodes[3];
+  piece.silver_coin = piece.childNodes[3].childNodes[1];
+  this.pieces[index] = piece;
+  
+  piece.white_light.setAttribute('opacity', '0.0');
+  piece.black_light.setAttribute('opacity', '0.0');
+  piece.gold_coin.setAttribute('opacity', '0.0');
+  piece.silver_coin.setAttribute('opacity', '0.0');
 
-Board.prototype.turn_light_on = function(piece) {
-  if(this.waits) return;
-  if(this.place_value(piece.index) == 2) return;
-  if(this.stage == SET_STAGE) {
-    if(this.place_value(piece.index) == 1) return;
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '1.0');
-  } else {
-    if(this.place_value(piece.index) == 1) {
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.5');
-      piece.childNodes[3].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.5');
-    } else
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '1.0');
-  }
-  piece.childNodes[3].childNodes[3 - this.player_mode*2].setAttribute('opacity', '1.0');
-}
+  piece.on_mouse_moved = function(isOver) {
+    if(board.waits) return;
+    if(this.value == 2) return;
+    if(board.stage == SET_STAGE && this.value == 1) return;
 
-Board.prototype.turn_light_off = function(piece) {
-  if(this.waits) return;
-  if(this.place_value(piece.index) == 2) return;
-  if(this.stage == SET_STAGE) {
-    if(this.place_value(piece.index) == 1) return;
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.0');
-  } else {
-    if(this.place_value(piece.index) == 1) {
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.0');
-      piece.childNodes[3].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.5');
-    } else
-      piece.childNodes[1].childNodes[1].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.0');
+    var light = (board.player_mode == WHITE_PLAYER ? this.white_light : this.black_light);
+    var coin  = (board.player_mode == WHITE_PLAYER ? this.gold_coin : this.silver_coin);
+
+    if(board.stage == MOVE_STAGE && this.value == 1)
+      var opacity = isOver ? '0.5' : '1.0';
+    else
+      var opacity = isOver ? '1.0' : '0.0';
+
+    light.setAttribute('opacity', opacity);
+    coin.setAttribute('opacity', opacity);
   }
-  piece.childNodes[3].childNodes[3 - this.player_mode*2].setAttribute('opacity', '0.0');
+
+  piece.onmouseover = function() { this.on_mouse_moved(true); }
+  piece.onmouseout = function() { this.on_mouse_moved(false); }
+  piece.onclick = function() {
+    if(board.waits) return;
+    if(board.stage == SET_STAGE && this.value == 0) {
+      this.value = 1;
+      (board.player_mode == WHITE_PLAYER ? this.white_light : this.black_light).setAttribute('opacity', '0.0');
+      (board.player_mode == WHITE_PLAYER ? this.gold_coin : this.silver_coin).setAttribute('opacity', '1.0');
+    }
+    //else if (this.value == 1) board.select(this);
+    //else if (this.value == 0) board.moveTo(this);
+  }
 }
 
 var board = new Board();
-
-function reset_piece(piece, index) {
-  piece.index = index;
-  for(var player_index = 1; player_index < 5; player_index += 2) {
-    piece.childNodes[1].childNodes[1].childNodes[player_index].setAttribute('opacity', '0.0');
-    piece.childNodes[3].childNodes[player_index].setAttribute('opacity', '0.0');
-  }
-}
-
 
